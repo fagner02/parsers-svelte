171c75d9e709399572b309c043e7a3e557958c5e
fagner ferreira <fagnerrc12@gmail.com>
2024-05-12 01:30:36 -0300

    update first animation

--- src/routes/StepsView.svelte
+++ src/routes/StepsView.svelte
@@ -1,21 +1,93 @@
 <script>
 	import { getTextWidth, wait } from '$lib/textwidth';
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
-	let grammar = 'S -> A B\nA -> a a\nB -> bb';
+	async function selectLSymbol(card, index1, color, empty) {
+		let symbol = document.querySelector(`#${card}l${index1}`);
+		symbol.classList.add(empty ? 'empty' : 'block');
+		symbol.classList.add(color);
+		await wait(500);
+	}
+
+	async function selectRSymbol(card, index1, index2, color, empty) {
+		let symbol = document.querySelector(`#${card}r${index1}-${index2}`);
+		symbol.classList.add(empty ? 'empty' : 'block');
+		symbol.classList.add(color);
+		await wait(500);
+	}
+
+	/**
+	 * @param {Array<number>} ruleStack
+	 */
+	async function addToSymbolStack(ruleStack) {
+		let currentRule = ruleStack[ruleStack.length - 1];
+		await selectLSymbol('g', currentRule, 'blue', false);
+
+		firstIndexes.set(rules[currentRule].left, first.length);
+		first = [...first, { left: rules[currentRule].left, right: [' '], showRight: false }];
+
+		firstCard.style.height = `${lineHeight * first.length}px`;
+		firstCard.style.maxWidth = `${firstCard.scrollWidth}px`;
+		await wait(0);
+
+		await selectLSymbol('f', currentRule, 'blue', false);
+
+		first[firstIndexes.get(rules[currentRule].left)].showRight = true;
+		await wait(0);
+		firstCard.style.maxWidth = `${firstCard.scrollWidth}px`;
+		await wait(0);
+	}
+
+	async function addToFirst(symbols, index) {
+		if (first[index].right[0] === ' ') {
+			first[index].right = symbols;
+		} else {
+			for (let i = 0; i < symbols.length; i++) {
+				if (!first[index].right.includes(symbols[i])) {
+					first[index].right = [...first[index].right, symbols[i]];
+					await wait(0);
+					console.log('added');
+					firstCard.style.maxWidth = `${firstCard.scrollWidth}px`;
+					await wait(1000);
+				}
+			}
+		}
+	}
+
+	export function firstSpanId(length, index1, index2) {
+		return `fr${index1}-${index2}`;
+	}
+
+	/**
+	 * @param {Array<number>} ruleStack
+	 * @param {Array<number>} prodStack
+	 * @returns {Promise<string | null>} symbol
+	 */
+	async function nextProdSymbol(ruleStack, prodStack) {
+		let currentRule = ruleStack[ruleStack.length - 1];
+		prodStack[prodStack.length - 1]++;
+		let prodPos = prodStack[prodStack.length - 1];
+		if (prodPos >= rules[currentRule].right.length) {
+			return null;
+		}
+		await selectRSymbol('g', currentRule, prodPos, 'green', true);
+		return rules[currentRule].right[prodPos];
+	}
+
+	let grammar = 'S -> A Bb\nA -> a a\nBb -> b m';
 	/**
 	 * @type {Array.<{left: string, right: Array.<string>}>}
-	 *
 	 */
 	export let rules = [];
 	export let fontSize = 15;
-	export let lineHeight = 23;
+	export let lineHeight = 26;
 	export let charWidth = 0;
-	export let floatingPadding = { h: 5, v: 2 };
+	let nt = ['S', 'A', 'Bb'];
 
-	let maxfirst = { index: 0, value: 0 };
-	let maxrule = { index: 0, value: 0 };
-	console.log(rules);
+	let firstIndexes = new Map();
+	/** @type {HTMLElement}*/
+	let firstCard;
 
 	onMount(async () => {
 		charWidth = getTextWidth('0', fontSize);
@@ -24,97 +96,54 @@
 			let s = r.split('->');
 
 			if (s.length > 1) {
-				rules.push({ left: s[0].replaceAll(' ', ''), right: s[1].replaceAll(' ', '').split('') });
-				console.log(rules.length);
-				if (rules[rules.length - 1].right.length > maxrule.value) {
-					maxrule = { index: rules.length - 1, value: rules[rules.length - 1].right.length };
-				}
+				rules.push({ left: s[0].replaceAll(' ', ''), right: s[1].trim().split(' ') });
 			}
 		});
-		console.log(rules);
-		document.querySelector('#grammar').style.height = `${lineHeight * rules.length}px`;
 
-		// document.querySelector('#grammar').style.width =
-		// 	`${getTextWidth(`${rules[maxrule.index].left} -> ${rules[maxrule.index].right.join('')}`, fontSize) + 5}px`;
-
-		first.push({ left: 'S', right: [], showRight: false, whole: [] });
-		console.log(first);
-		document.querySelector('.card.first-card').style.height = `${lineHeight * first.length}px`;
-		// document.querySelector('.card.first-card').style.width =
-		// 	`${getTextWidth(`${first[maxfirst.index].left}: { ${first[maxfirst.index].right.join('')} }`, fontSize) + 5}px`;
-		document.querySelector('.card.first-card').style.maxWidth = '0px';
-		await wait(300);
-		// first = first;
+		let grammarCard = document.querySelector('#grammar');
+		grammarCard.style.maxWidth = '0px';
 		rules = rules;
 		await wait(200);
-		// document.querySelector('#l0').style.background = 'hsl(200, 50%, 50%)';
-		document.querySelector('#l0').classList.add('block');
-		document.querySelector('#l0').classList.add('blue');
-		// let rect = document.querySelector('#l0').getBoundingClientRect();
-		// let parent = document.querySelector('.steps').getBoundingClientRect();
-		// let floating = document.querySelector('.floating');
-		// floating.style.opacity = '1';
-		// floating.style.top = `${rect.top - parent.top - floatingPadding.v}px`;
-		// floating.style.left = `${rect.left - parent.left - floatingPadding.h}px`;
 
-		await wait(300);
-		first = first;
-		await wait(100);
-		console.log(`${document.querySelector('.card.first-card').innerHTML}px`);
-		document.querySelector('.card.first-card').style.maxWidth =
-			`${document.querySelector('.card.first-card').scrollWidth}px`;
+		grammarCard.style.height = `${lineHeight * rules.length}px`;
+		grammarCard.style.maxWidth = `${grammarCard.scrollWidth}px`;
 
-		await wait(300);
-		// rect = document.querySelector('#ls').getBoundingClientRect();
+		firstCard = document.querySelector('.card.first-card');
 
-		await wait(500);
-		// floating.style.top = `${rect.top - parent.top - floatingPadding.v}px`;
-		// floating.style.left = `${rect.left - parent.left - floatingPadding.h}px`;
-		// await wait(500);
-		// floating.style.opacity = 0;
+		let ruleStack = [0];
+		let prodStack = [-1];
 
-		await wait(500);
-		// floating.style.display = 'none';
+		while (ruleStack.length > 0) {
+			if (!firstIndexes.has(rules[ruleStack[ruleStack.length - 1]]?.left)) {
+				await addToSymbolStack(ruleStack, firstIndexes);
+			}
 
-		first[first.length - 1].showRight = true;
+			while (true) {
+				let symbol = await nextProdSymbol(ruleStack, prodStack);
 
-		await wait(0);
-
-		document.querySelector('.card.first-card').style.maxWidth =
-			`${document.querySelector('.card.first-card').scrollWidth}px`;
-		await wait(500);
-		const left = [':', '{', ' '];
-		const right = ['}'];
-
-		for (let i = 0; i < left.length; i++) {
-			first[first.length - 1].whole = [...first[first.length - 1].whole, left[i]];
-
-			await wait(0);
-			document.querySelector('.card.first-card').style.maxWidth =
-				`${document.querySelector('.card.first-card').scrollWidth}px`;
-			await wait(100);
+				if (symbol === null) {
+					prodStack.pop();
+					let topRule = rules[ruleStack.pop()];
+					let lastRule = rules[ruleStack[ruleStack.length - 1]];
+					if (ruleStack.length > 0) {
+						await addToFirst(
+							first[firstIndexes.get(topRule.left)].right,
+							firstIndexes.get(lastRule.left)
+						);
+					}
+					break;
+				}
+				if (nt.includes(symbol)) {
+					ruleStack.push(rules.findIndex((x) => x.left === symbol));
+					prodStack.push(-1);
+					break;
+				} else {
+					let index = firstIndexes.get(rules[ruleStack[ruleStack.length - 1]].left);
+					await addToFirst([symbol], index);
+					continue;
+				}
+			}
 		}
-		for (let i = 0; i < right.length; i++) {
-			first[first.length - 1].whole = [...first[first.length - 1].whole, right[i]];
-
-			await wait(0);
-			document.querySelector('.card.first-card').style.maxWidth =
-				`${document.querySelector('.card.first-card').scrollWidth}px`;
-			await wait(100);
-		}
-		// let  = [':', '{', '}'];
-		await wait(500);
-
-		// for (let i = 0; i < 3; i++) {
-
-		// 	first[first.length - 1].whole = [left, ...first[first.length - 1].right, right];
-
-		// 	await wait(0);
-		// 	document.querySelector('.card.first-card').style.maxWidth =
-		// 		`${document.querySelector('.card.first-card').scrollWidth}px`;
-		// 	await wait(100);
-		// }
-		// let parent = document.querySelector('.steps').getBoundingClientRect();
 	});
 
 	/**
@@ -126,10 +155,14 @@
 <div class="steps {$$props.class}" style="position: relative;">
 	<div class="card" id="grammar">
 		{#each rules as rule, i}
-			<p style="line-height: {lineHeight}px; font-size: {fontSize}px">
-				<span id="l{i}">{rule.left}</span>
+			<p
+				style="line-height: {lineHeight}px; font-size: {fontSize}px; padding: 0px; width: fit-content"
+			>
+				<span id="gl{i}">{rule.left}</span>
 				<span>{'->'}</span>
-				<span>{rule.right.join('')}</span>
+				{#each rule.right as symbol, si}
+					<span id="gr{i}-{si}">{symbol}</span>
+				{/each}
 			</p>
 		{/each}
 	</div>
@@ -139,40 +172,54 @@
 				id="fset{index}"
 				style="line-height: {lineHeight}px;font-size:{fontSize}px; padding: 0px; width: fit-content"
 			>
-				<span id="ls">{f.left}</span>
+				<span id="fl{index}">{f.left}</span>
 				{#if f.showRight}
-					{#each [...f.whole] as text}
-						<span
-							>{#if text != ' '}
-								{text}
-							{:else}
-								&nbsp;
-							{/if}</span
-						>
+					<span in:fade={{ delay: 0 }}>{':'}</span>
+					<span in:fade={{ delay: 100 }}>{'{'}</span>
+
+					{#each f.right as text, ri}
+						{#key text}
+							{#await wait(100) then}
+								<span id={firstSpanId(f.right.length, index, ri)}>
+									{#if text != ' '}
+										{text}
+									{:else}
+										&nbsp;
+									{/if}</span
+								>
+							{/await}
+							{#await wait(100) then}
+								{#if ri < f.right.length - 1}
+									<span>,</span>
+								{/if}
+							{/await}
+						{/key}
 					{/each}
+
+					<span in:fade={{ delay: 300 }}>{'}'}</span>
 				{/if}
 			</p>
 		{/each}
 	</div>
-
-	<!-- <p class="floating" style="padding: {floatingPadding.v}px {floatingPadding.h}px">S</p> -->
 </div>
 
 <style>
 	.card {
-		width: 0px;
 		height: 0px;
 		background: white;
 		box-shadow: 0px 0px 5px 0px hsl(0, 0%, 0%, 30%);
 		border-radius: 10px;
 		padding: 5px 10px;
 		margin: 5px;
-		transition:
-			width 0.3s,
-			height 0.3s;
+		transition: width 0.3s;
 		overflow: hidden;
 		width: fit-content;
-		transition: max-width 0.5s;
+		max-width: 0px;
+		transition:
+			max-width 0.5s,
+			width 0.3s,
+			height 0.3s;
+		text-wrap: nowrap;
 	}
 
 	.steps {
@@ -188,8 +235,6 @@
 		color: white;
 		box-shadow: 0px 0px 5px 0px hsl(0, 0%, 0%, 30%);
 		border-radius: 6px;
-		/* opacity: 0; */
-		/* padding: ; */
 		transition:
 			top 0.5s,
 			left 0.5s,
@@ -201,14 +246,28 @@
 		transition: color 0.3s;
 	}
 
-	.first-row {
-		display: flex;
+	.first-card > p > span:nth-child(2) {
+		padding-left: 5px;
+	}
+
+	#grammar > p > span:nth-child(2) {
+		padding: 0px;
+		padding-left: 5px;
+	}
+
+	#grammar > p > span:first-child {
+		padding: 0px;
+	}
+
+	#grammar > p > span {
+		padding: 0px 5px;
 	}
 
 	.first-card > p {
 		padding: 0px;
 		display: flex;
 	}
+
 	@keyframes appear {
 		from {
 			opacity: 0;
@@ -217,14 +276,13 @@
 			opacity: 1;
 		}
 	}
-	.first-card > p > span {
-		/* padding: 0px;
-		display: flex; */
-		/* opacity: 0; */
-		/* transition: opacity 0.5s; */
+
+	.card > p > span {
 		animation: appear 1s;
 		z-index: 1;
+		position: relative;
 	}
+
 	@keyframes rotateAppear {
 		from {
 			transform: perspective(45px) rotateY(90deg);
@@ -233,11 +291,13 @@
 			transform: perspective(45px) rotateY(0deg);
 		}
 	}
+
 	.block {
 		position: relative;
 		z-index: 0;
 		color: white !important;
 	}
+
 	.block::after {
 		content: '';
 		position: absolute;
@@ -258,9 +318,10 @@
 
 	.empty {
 		position: relative;
-		z-index: 0;
-		color: black !important;
+		z-index: 0 !important;
+		color: white !important;
 	}
+
 	.empty::after {
 		content: '';
 		position: absolute;
@@ -268,8 +329,8 @@
 		border-radius: 5px;
 		transform: perspective(45px);
 		--width: 100%;
-		width: calc(var(--width) + 8px);
-		--height: 15px;
+		width: calc(var(--width) - 2px);
+		--height: 20px;
 		height: var(--height);
 		top: 50%;
 		translate: calc(var(--width) / -2) calc(var(--height) / -2);
@@ -281,9 +342,13 @@
 
 	.blue::after {
 		background: hsl(200, 60%, 50%);
+
+		box-shadow: 0px 0px 5px 0px hsl(200, 60%, 55%, 90%);
 	}
 	.green::after {
 		background: hsl(100, 60%, 50%);
+
+		box-shadow: 0px 0px 5px 0px hsl(100, 60%, 55%, 70%);
 	}
 	.yellow::after {
 		background: hsl(60, 60%, 50%);
