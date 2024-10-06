c4032c2a0dcf6442722bf6aa6540d433c77bde51
fagner ferreira <fagnerrc12@gmail.com>
2024-10-06 15:55:06 -0300

    update

--- static/app.js
+++ static/app.js
@@ -5,3 +5,17 @@
 // 	.getElementById('titlebar-maximize')
 // 	?.addEventListener('click', () => appWindow.toggleMaximize());
 // document.getElementById('titlebar-close')?.addEventListener('click', () => appWindow.close());
+window.onload = (eev) => {
+	console.log(document.getElementsByTagName('style'));
+
+	const divs = document.getElementsByTagName('div');
+	for (let div of divs) {
+		console.log(div.attributes.getNamedItem('name')?.value === 'discard');
+		if (div.attributes.getNamedItem('name')?.value === 'discard') {
+			div.style.opacity = '0';
+			div.style.translate = '0px -50px';
+			setTimeout(() => div.remove(), 1000);
+			break;
+		}
+	}
+};
