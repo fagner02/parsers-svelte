bd889c5c4d10d251ac373f3b4e6878c8a3622b71
fagner ferreira <fagnerrc12@gmail.com>
2024-11-03 22:23:26 -0300

    update

--- src/lib/components/FollowAlgorithm.svelte
+++ src/lib/components/FollowAlgorithm.svelte
@@ -16,6 +16,8 @@
 	import { onMount } from 'svelte';
 	import { getSelectionFunctions } from '@/Cards/selectionFunction';
 	import { getGrammar } from '$lib/utils';
+	import PseudoCode from './PseudoCode.svelte';
+	import { circIn } from 'svelte/easing';
 
 	/**@type {SetsCard | undefined}*/
 	let followSetElement;
@@ -25,6 +27,8 @@
 	let joinStackElement;
 	/**@type {SvgLines | undefined}*/
 	let svgLines;
+	/**@type {PseudoCode}*/
+	let codeCard;
 	/**@type {() => Promise<void>}*/
 	let loadGrammar;
 	/**@type {string}*/
@@ -67,41 +71,56 @@
 			await loadGrammar();
 			await addPause();
 			instruction = 'Since this thing is like that we have add to the stack';
+			await codeCard.highlightLines([0, 1]);
 			if (currentlyRunning != id) return;
 			await followSetElement?.addSetRow(rules[0].left, rules[0].left);
+			await codeCard.highlightLines([2]);
 			if (currentlyRunning != id) return;
 			await followSetElement?.joinSets(['$'], ['$'], null, rules[0].left);
 
 			for (let i = 0; i < rules.length; i++) {
+				await codeCard.highlightLines([3]);
 				if (currentlyRunning != id) return;
 				await grammarFuncs?.selectFor(`gset${i}`);
 				for (let j = 0; j < rules[i].right.length; j++) {
+					await codeCard.highlightLines([4]);
 					if (currentlyRunning != id) return;
-
 					const symbol = rules[i].right[j];
+
+					await codeCard.highlightLines([5]);
 					let followingSymbolIndex = j + 1;
 					let followingSymbol = j + 1 == rules[i].right.length ? null : rules[i].right[j + 1];
 
+					await codeCard.highlightLines([6]);
 					if (!nt.includes(symbol)) {
+						await codeCard.highlightLines([7]);
 						if (currentlyRunning != id) return;
 						await selectRSymbol('g', i, j, colors.green, false);
 						continue;
 					}
+					await codeCard.highlightLines([8]);
 					if (currentlyRunning != id) return;
 					await selectRSymbol('g', i, j, colors.blue, false);
 
 					if (!followIndexes.has(symbol)) {
+						await codeCard.highlightLines([9]);
 						if (currentlyRunning != id) return;
 						await followSetElement?.addSetRow(symbol, symbol, `gr${i}-${j}`);
 					}
 
+					await codeCard.highlightLines([10]);
 					let pos = 1;
 					while (true) {
+						await codeCard.highlightLines([11]);
+						await codeCard.highlightLines([12]);
 						if (followingSymbol === null) {
+							await codeCard.highlightLines([13]);
 							if (!joinSetElement?.has(symbol)) {
+								await codeCard.highlightLines([14]);
 								if (currentlyRunning != id) return;
 								await joinSetElement?.addSetRow(symbol, symbol, `gr${i}-${j}`);
 							}
+							await codeCard.highlightLines([15]);
 							if (currentlyRunning != id) return;
 							await joinSetElement?.joinSets(
 								[rules[i].left],
@@ -110,18 +129,27 @@
 								symbol,
 								`gl${i}`
 							);
+							await codeCard.highlightLines([16]);
 							break;
 						}
+						await codeCard.highlightLines([17]);
 						if (currentlyRunning != id) return;
 						await selectRSymbol('g', i, j + 1, colors.orange, false);
 						if (nt.includes(followingSymbol)) {
+							await codeCard.highlightLines([18]);
 							let empty = false;
+
 							for (let [key, item] of $firstSet.entries()) {
+								await codeCard.highlightLines([19]);
+								await codeCard.highlightLines([20]);
 								if (item.left === followingSymbol) {
+									await codeCard.highlightLines([21]);
 									if (item.right.includes('')) {
+										await codeCard.highlightLines([22, 23]);
 										empty = true;
 										continue;
 									}
+									await codeCard.highlightLines([24, 25]);
 									if (currentlyRunning != id) return;
 									await followSetElement?.joinSets(
 										item.right,
@@ -132,15 +160,20 @@
 									);
 								}
 							}
-
+							await codeCard.highlightLines([26]);
 							if (!empty) {
+								await codeCard.highlightLines([27]);
 								break;
 							}
+							await codeCard.highlightLines([28, 29]);
 							followingSymbolIndex = j + 1 + pos;
 							followingSymbol =
 								j + 1 + pos == rules[i].right.length ? null : rules[i].right[j + 1 + pos];
 							pos++;
+							await codeCard.highlightLines([30]);
 						} else {
+							await codeCard.highlightLines([30]);
+							await codeCard.highlightLines([31]);
 							if (currentlyRunning != id) return;
 							await followSetElement?.joinSets(
 								[followingSymbol],
@@ -149,6 +182,7 @@
 								symbol,
 								`gr${i}-${followingSymbolIndex}`
 							);
+							await codeCard.highlightLines([32]);
 							break;
 						}
 					}
@@ -158,9 +192,14 @@
 			}
 			grammarFuncs?.hideSelect();
 			for (let item of joinIndexes.keys()) {
+				await codeCard.highlightLines([33]);
+				await codeCard.highlightLines([34]);
 				if (joinSetElement?.get(item)?.length === 0) {
+					await codeCard.highlightLines([35]);
 					continue;
 				}
+				await codeCard.highlightLines([36]);
+				await codeCard.highlightLines([37]);
 				if (currentlyRunning != id) return;
 				await joinStackElement?.addToStack(
 					item,
@@ -172,11 +211,15 @@
 				await addPause();
 
 				while ($joinStack.length > 0) {
+					await codeCard.highlightLines([38]);
+					await codeCard.highlightLines([39, 40]);
 					const topKey = $joinStack[$joinStack.length - 1].data;
 					const top = /**@type {Array<string>}*/ (joinSetElement?.get(topKey));
 
+					await codeCard.highlightLines([41]);
 					let nextSet = joinSetElement?.get(top[0]);
 					if (nextSet !== undefined && !(nextSet.length === 0)) {
+						await codeCard.highlightLines([42]);
 						if (currentlyRunning != id) return;
 						await joinStackElement?.addToStack(
 							top[0],
@@ -185,8 +228,10 @@
 							top[0],
 							`${joinSetElement?.getSetId()}l${joinIndexes.get(top[0])}`
 						);
+						await codeCard.highlightLines([43]);
 						continue;
 					}
+
 					if (currentlyRunning != id) return;
 					await selectRSymbol(
 						/**@type {string}*/ (joinSetElement?.getSetId()),
@@ -196,7 +241,7 @@
 					);
 
 					const setToJoin = /**@type {Array<string>}*/ (followSetElement?.get(top[0]));
-
+					await codeCard.highlightLines([44, 45]);
 					if (currentlyRunning != id) return;
 					await followSetElement?.joinSets(
 						setToJoin,
@@ -205,11 +250,14 @@
 						topKey,
 						`${followSetElement.getSetId()}l${followIndexes.get(top[0])}`
 					);
+					await codeCard.highlightLines([46]);
 
 					if (currentlyRunning != id) return;
 					await joinSetElement?.remove(topKey, top[0]);
 
+					await codeCard.highlightLines([47]);
 					if (joinSetElement?.get(topKey)?.length === 0) {
+						await codeCard.highlightLines([48]);
 						if (currentlyRunning != id) return;
 						await joinStackElement?.removeFromStack($joinStack.length - 1);
 					}
@@ -223,12 +271,14 @@
 		}
 	}
 
-	onMount(() => {
+	onMount(async () => {
 		grammarFuncs = getSelectionFunctions('g');
+		codeCard.setPseudoCode(await (await fetch('./follow.txt')).text());
 		reset();
 	});
 </script>
 
+<PseudoCode bind:this={codeCard}></PseudoCode>
 <SvgLines svgId="follow-svg" bind:this={svgLines}></SvgLines>
 <div class="cards-box unit">
 	<GrammarCard bind:loadGrammar></GrammarCard>
