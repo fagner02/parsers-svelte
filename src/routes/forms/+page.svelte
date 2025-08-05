90df6a1883d00d86d4a10f0493e7ee69c0c36500
Fagner Ferreira <fagnerrc12@gmail.com>
2025-08-04 22:51:42 -0300

    update form

--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -112,7 +112,7 @@
 						</div>
 						<hr />
 						<div class="field col">
-							3. De 1 a 5, quanto a interface da ferramenta foi fácil de navegar?
+							3. De 1 a 5, o quão fácil de navegar foi a interface da ferramenta?
 							<div class="range-box">
 								<input type="range" value="0" id="q3" min="1" max="5" name="q3" list="values" />
 								<div class="values-box">
@@ -126,7 +126,7 @@
 						</div>
 						<hr />
 						<div class="field col">
-							4. Os elementos da interface (botões, janelas, tooltips) foram claros em sua função?
+							4. Os elementos da interface (botões, janelas) foram claros em sua função?
 							<div class="row">
 								<div>
 									<input type="radio" name="q4" value="Sim, todos" /><label for="q4"
@@ -150,8 +150,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							5. De 1 a 5, quanto os tooltips (descrições que aparecem ao passar o mouse por cima de
-							botões) foram úteis?
+							5. De 1 a 5, o quão útil foram os tooltips (descrições que aparecem ao passar o mouse
+							por cima de botões)?
 							<div class="range-box">
 								<input type="range" value="0" id="q5" min="1" max="5" name="q5" list="values" />
 								<div class="values-box">
@@ -165,8 +165,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							6. De 1 a 5, como você avalia a organização das abas (gramática, algoritmos, análise
-							de string)?
+							6. De 1 a 5, o quão útil foi a possibilidade de mover livremente os elementos de
+							pseudocódigo/autômato?
 							<div class="range-box">
 								<input type="range" value="0" id="q6" min="1" max="5" name="q6" list="values" />
 								<div class="values-box">
@@ -180,8 +180,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							7. De 1 a 5, quanto a visualização passo a passo dos algoritmos ajudou a entender seu
-							funcionamento?
+							7. De 1 a 5, o quão útil foram as ações de minimizar/abrir os elementos de
+							pseudocódigo/autômato?
 							<div class="range-box">
 								<input type="range" value="0" id="q7" min="1" max="5" name="q7" list="values" />
 								<div class="values-box">
@@ -195,8 +195,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							8. De 1 a 5, quanto as animações (destaques, transições, movimentação de dados) foram
-							úteis para acompanhar as mudanças nas estruturas de dados?
+							8. De 1 a 5, como você avalia a organização das abas (abas de gramática, algoritmos e
+							análise de string)?
 							<div class="range-box">
 								<input type="range" value="0" id="q8" min="1" max="5" name="q8" list="values" />
 								<div class="values-box">
@@ -210,8 +210,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							9. De 1 a 5, quanto a visualização do autômato (itens canônicos) foi clara e ajudou no
-							entendimento dos algoritmos SLR e LR(1)?
+							9. De 1 a 5, quanto a visualização passo a passo dos algoritmos ajudou a entender seu
+							funcionamento?
 							<div class="range-box">
 								<input type="range" value="0" id="q9" min="1" max="5" name="q9" list="values" />
 								<div class="values-box">
@@ -225,8 +225,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							10. De 1 a 5, quanto a visualização da árvore sintática ajudou no entendimento da
-							análise sintática de strings?
+							10. De 1 a 5, quanto as animações (destaques, transições, movimentação de dados) foram
+							úteis para acompanhar as mudanças nas estruturas de dados?
 							<div class="range-box">
 								<input type="range" value="0" id="q10" min="1" max="5" name="q10" list="values" />
 								<div class="values-box">
@@ -240,8 +240,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							11. De 1 a 5, quanto a possibilidade de avançar, voltar e reiniciar os passos foi
-							importante para o aprendizado?
+							11. De 1 a 5, quanto a visualização do autômato (itens canônicos) foi clara e ajudou
+							no entendimento dos algoritmos SLR e LR(1)?
 							<div class="range-box">
 								<input type="range" value="0" id="q11" min="1" max="5" name="q11" list="values" />
 								<div class="values-box">
@@ -255,8 +255,8 @@
 						</div>
 						<hr />
 						<div class="field col">
-							12. De 1 a 5, quanto o pseudocódigo com breakpoints ajudou a relacionar a teoria com a
-							execução?
+							12. De 1 a 5, quanto a visualização da árvore sintática ajudou no entendimento da
+							análise sintática de strings?
 							<div class="range-box">
 								<input type="range" value="0" id="q12" min="1" max="5" name="q12" list="values" />
 								<div class="values-box">
@@ -270,64 +270,67 @@
 						</div>
 						<hr />
 						<div class="field col">
-							13. Em geral, como você avalia a ferramenta VANSI como apoio ao aprendizado de análise
+							13. De 1 a 5, quanto a possibilidade de avançar, voltar e reiniciar os passos foi
+							importante para o aprendizado?
+							<div class="range-box">
+								<input type="range" value="0" id="q13" min="1" max="5" name="q13" list="values" />
+								<div class="values-box">
+									<p>1</p>
+									<p>2</p>
+									<p>3</p>
+									<p>4</p>
+									<p>5</p>
+								</div>
+							</div>
+						</div>
+						<hr />
+						<div class="field col">
+							14. De 1 a 5, quanto o pseudocódigo com breakpoints ajudou a relacionar a teoria com a
+							execução?
+							<div class="range-box">
+								<input type="range" value="0" id="q14" min="1" max="5" name="q14" list="values" />
+								<div class="values-box">
+									<p>1</p>
+									<p>2</p>
+									<p>3</p>
+									<p>4</p>
+									<p>5</p>
+								</div>
+							</div>
+						</div>
+						<hr />
+						<div class="field col">
+							15. Em geral, como você avalia a ferramenta VANSI como apoio ao aprendizado de análise
 							sintática?
 							<div class="row">
 								<div>
-									<input type="radio" name="q13" value="Excelente" /><label for="q13"
+									<input type="radio" name="q15" value="Excelente" /><label for="q15"
 										>Excelente</label
 									>
 								</div>
 								<div>
-									<input type="radio" name="q13" value="Boa" /><label for="q13">Boa</label>
+									<input type="radio" name="q15" value="Boa" /><label for="q15">Boa</label>
 								</div>
 								<div>
-									<input type="radio" name="q13" value="Regular" /><label for="q13">Regular</label>
+									<input type="radio" name="q15" value="Regular" /><label for="q15">Regular</label>
 								</div>
 								<div>
-									<input type="radio" name="q13" value="Ruim" /><label for="q13">Ruim</label>
+									<input type="radio" name="q15" value="Ruim" /><label for="q15">Ruim</label>
 								</div>
 								<div>
-									<input type="radio" name="q13" value="Péssima" /><label for="q13">Péssima</label>
+									<input type="radio" name="q15" value="Péssima" /><label for="q15">Péssima</label>
 								</div>
 							</div>
 						</div>
 						<hr />
 						<div class="field col">
-							14. Você usaria essa ferramenta novamente para estudar algoritmos de compiladores?
-							<div class="row">
-								<div>
-									<input type="radio" name="q14" value="Sim" /><label for="q14">Sim</label>
-								</div>
-								<div>
-									<input type="radio" name="q14" value="Talvez" /><label for="q14">Talvez</label>
-								</div>
-								<div>
-									<input type="radio" name="q14" value="Não" /><label for="q14">Não</label>
-								</div>
-							</div>
-						</div>
-						<hr />
-						<div class="field col">
-							15. Recomendaria a ferramenta para outros alunos?
-							<div class="row">
-								<div>
-									<input type="radio" name="q15" value="Sim" /><label for="q15">Sim</label>
-								</div>
-								<div>
-									<input type="radio" name="q15" value="Talvez" /><label for="q15">Talvez</label>
-								</div>
-								<div>
-									<input type="radio" name="q15" value="Não" /><label for="q15">Não</label>
-								</div>
-							</div>
-						</div>
-						<hr />
-						<div class="field col">
-							16. Você já usou a ferramenta antes?
+							16. Você usaria essa ferramenta novamente para estudar algoritmos de compiladores?
 							<div class="row">
 								<div>
 									<input type="radio" name="q16" value="Sim" /><label for="q16">Sim</label>
+								</div>
+								<div>
+									<input type="radio" name="q16" value="Talvez" /><label for="q16">Talvez</label>
 								</div>
 								<div>
 									<input type="radio" name="q16" value="Não" /><label for="q16">Não</label>
@@ -336,9 +339,38 @@
 						</div>
 						<hr />
 						<div class="field col">
-							17. Sugestões ou comentários adicionais sobre a ferramenta
-							<textarea data-optional="true" name="q16" id="q16"></textarea>
+							17. Recomendaria a ferramenta para outros alunos aprendendo sobre analisadores
+							sintáticos?
+							<div class="row">
+								<div>
+									<input type="radio" name="q17" value="Sim" /><label for="q17">Sim</label>
+								</div>
+								<div>
+									<input type="radio" name="q17" value="Talvez" /><label for="q17">Talvez</label>
+								</div>
+								<div>
+									<input type="radio" name="q17" value="Não" /><label for="q17">Não</label>
+								</div>
+							</div>
 						</div>
+						<hr />
+						<div class="field col">
+							18. Você já usou a ferramenta antes?
+							<div class="row">
+								<div>
+									<input type="radio" name="q18" value="Sim" /><label for="q18">Sim</label>
+								</div>
+								<div>
+									<input type="radio" name="q18" value="Não" /><label for="q18">Não</label>
+								</div>
+							</div>
+						</div>
+						<hr />
+						<div class="field col">
+							19. Sugestões, críticas ou comentários adicionais sobre a ferramenta:
+							<textarea data-optional="true" name="q19" id="q19"></textarea>
+						</div>
+						<hr />
 					</div>
 				{/snippet}
 			</FillSize>
@@ -351,7 +383,6 @@
 		border: 1px solid hsl(0, 0%, 0%, 20%);
 		height: inherit;
 		border-radius: 8px;
-		overflow: auto;
 	}
 
 	.fields {
