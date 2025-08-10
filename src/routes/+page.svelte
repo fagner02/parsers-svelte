9882c4a05aef3e05fd9ae42ca77f43ab09a951f0
Fagner Ferreira <fagnerrc12@gmail.com>
2025-08-10 13:50:33 -0300

    update loading animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
 <script>
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+	let isNavigating = false;
+
+	beforeNavigate(() => (isNavigating = true));
+	afterNavigate(() => (isNavigating = false));
+	import { noJumpWait } from '$lib/flowControl';
 	import Tabs from '@/Tabs/Tabs.svelte';
 	import { AndroidFullScreen } from '@awesome-cordova-plugins/android-full-screen';
 	AndroidFullScreen.isImmersiveModeSupported()
@@ -11,6 +17,21 @@
 	if (params.get('type') === 'recovery') {
 		window.location.href = `/reset-password#${window.location.hash.substring(1)}`;
 	}
+	(async () => {
+		while (isNavigating) {
+			await noJumpWait(100);
+		}
+
+		let div = /**@type {HTMLElement}*/ (document.querySelector("div[name='discard']"));
+		if (div === null) return;
+
+		await noJumpWait(100);
+		div.style.opacity = '0';
+		div.style.transform = 'translate(0px, -20px)';
+		await noJumpWait(1000);
+		div.remove();
+		return;
+	})();
 </script>
 
 <Tabs class="unit "></Tabs>
--- static/app.js
+++ static/app.js
@@ -1,28 +1,4 @@
-/** @param {number} ms */
-async function wait(ms) {
-	return new Promise((resolve, _) => {
-		setTimeout(resolve, ms);
-	});
-}
 const font = new FontFace('spacemono', 'url(/fonts/SpaceMono-Regular.ttf)');
 document.fonts.add(font);
 
 font.load();
-window.onload = async (_) => {
-	let div = document.querySelector("div[name='discard']");
-	if (div === null) return;
-	while (true) {
-		if (
-			document.head.querySelectorAll('link').length > 1 ||
-			document.head.querySelectorAll('style').length > 1
-		) {
-			await wait(100);
-			div.style.opacity = '0';
-			div.style.transform = 'translate(0px, -20px)';
-			await wait(1000);
-			div.remove();
-			return;
-		}
-		await wait(100);
-	}
-};
